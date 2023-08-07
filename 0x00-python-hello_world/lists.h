1d8016d927113bb1fd3597d19bdacee6707d6157
eskadimasmelaku <eskadimasmelaku@gmail.com>
2023-08-07 15:55:58 -0700

    updated

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,69 +1,25 @@
-#include "lists.h"
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stdlib.h>
 
 /**
- * _realloc - Reallocates a memory block
- * @ptr: The pointer to the previous memory block
- * @old_size: The size of the old memory block
- * @new_size: The size of the new memory block
+ *struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  *
- * Return: The pointer to the new memory block otherwise NULL
- */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+ * Description: singly linked list node structure
+ * for Holberton project
+ **/
+typedef struct listint_s
 {
-	void *new_ptr;
-	unsigned int min_size = old_size < new_size ? old_size : new_size;
-	unsigned int i;
+	    int n;
+	        struct listint_s *next;
+} listint_t;
 
-	if (new_size == old_size)
-		return (ptr);
-	if (ptr != NULL)
-	{
-		if (new_size == 0)
-		{
-			free(ptr);
-			return (NULL);
-		}
-		new_ptr = malloc(new_size);
-		if (new_ptr != NULL)
-		{
-			for (i = 0; i < min_size; i++)
-				*((char *)new_ptr + i) = *((char *)ptr + i);
-			free(ptr);
-			return (new_ptr);
-		}
-		free(ptr);
-		return (NULL);
-	}
-	else
-	{
-		new_ptr = malloc(new_size);
-		return (new_ptr);
-	}
-}
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
 
-
-
-/**
- * check_cycle - checks if a singly linked list has a cycle in it
- * @list: list head
- * Return: 0 if no, 1 if yes
- */
-
-int check_cycle(listint_t *list)
-{
-	listint_t *fast, *slow = list;
-
-	if (list == NULL)
-		return (0);
-
-	fast = list->next;
-	while (slow != NULL && fast != NULL && fast->next != NULL)
-	{
-		if (slow == fast)
-			return (1);
-		fast = fast->next->next;
-		slow = slow->next;
-	}
-	return (0);
-}
+#endif /* LISTS_H */
