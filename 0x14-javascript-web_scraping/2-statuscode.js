c60315043d2bf837f4eab4068ef76ab876217eda
eskadimasmelaku <eskadimasmelaku@gmail.com>
2024-01-04 01:07:07 -0800

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,6 @@
+2-statuscode.js
 #!/usr/bin/node
 const request = require('request');
 request.get(process.argv[2]).on('response', function (response) {
   console.log(`code: ${response.statusCode}`);
-})
+});
